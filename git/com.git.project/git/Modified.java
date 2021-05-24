9e3e6d1f4b4a6cf61d030ea18117336f82ad34b5
ramyaasri <raamyaa.sri@gmail.com>
2021-05-24 12:51:10 +0530

    branch other

--- git/com.git.project/git/Modified.java
+++ git/com.git.project/git/Modified.java
@@ -3,8 +3,8 @@ package git;
 public class Modified {
 
 	public static void main(String[] args) {
-		System.out.println("This is Test Class");
-		System.out.println("Hi this is Ramya");
-		System.out.println("Created Branch");
+		
+		System.out.println("Created new  Branch other");
+		
 	}
 }
