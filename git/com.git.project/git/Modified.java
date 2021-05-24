7e46db3e10d32e05fe82760cdb429ed98c469ced
ramyaasri <raamyaa.sri@gmail.com>
2021-05-24 12:52:47 +0530

    Master

--- git/com.git.project/git/Modified.java
+++ git/com.git.project/git/Modified.java
@@ -5,5 +5,6 @@ public class Modified {
 	public static void main(String[] args) {
 		System.out.println("This is Test Class");
 		System.out.println("Hi this is Ramya");
+		System.out.println("This is master");
 	}
 }
